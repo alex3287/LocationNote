93d5d6a0a1fbeed8248b930fa87a33611540f4aa
Alexander Mangazeev <alex3287@bk.ru>
2021-05-17 17:21:22 +0700

    Initial Commit

---
+++ LocationNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationNote
+//
+//  Created by Александр Мангазеев on 17.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
